28005daf921b4a56ddc7f81dd8d05f7def4b3b64
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-05-05 19:59:50 -0400

    Initial commit

---
+++ site/src/jsMain/kotlin/io/github/opletter/css2kobweb/pages/Index.kt
@@ -0,0 +1,11 @@
+package io.github.opletter.css2kobweb.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.Page
+
+@Page
+@Composable
+fun HomePage() {
+    // TODO: Add content here
+    println("Hello World!")
+}
