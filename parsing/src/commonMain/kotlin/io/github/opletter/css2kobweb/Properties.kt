55ca9449a64f9b4b58255c628029f2cf711f33e2
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-05-14 16:52:49 -0400

    Improve property parsing don't split values within quotes or parens

--- parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/Properties.kt
+++ parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/Properties.kt
@@ -13,40 +13,70 @@ internal fun kebabToCamelCase(str: String): String {
     return kebabToPascalCase(str).replaceFirstChar { it.lowercase() }
 }
 
+internal data class ParseState(
+    val inQuotes: Boolean = false,
+    val inParentheses: Boolean = false,
+    val buffer: String = "",
+    val result: List<String> = emptyList(),
+)
+
+// split on comma or space (or both?), but not in parens or quotes; credit: ChatGPT
+internal tailrec fun splitString(input: String, state: ParseState = ParseState()): List<String> {
+    if (input.isEmpty()) {
+        return if (state.buffer.isNotEmpty()) state.result + state.buffer else state.result
+    }
+    val nextState = when (val ch = input.first()) {
+        '"' -> state.copy(inQuotes = !state.inQuotes, buffer = state.buffer + ch)
+        '(' -> state.copy(inParentheses = true, buffer = state.buffer + ch)
+        ')' -> state.copy(inParentheses = false, buffer = state.buffer + ch)
+        ' ', ',' -> {
+            if (state.inQuotes || state.inParentheses) {
+                state.copy(buffer = state.buffer + ch)
+            } else {
+                if (state.buffer.isNotEmpty()) {
+                    state.copy(buffer = "", result = state.result + state.buffer)
+                } else state
+            }
+        }
+
+        else -> state.copy(buffer = state.buffer + ch)
+    }
+    return splitString(input.drop(1), nextState)
+}
+
 internal fun parseValue(propertyName: String, value: String): List<Arg> {
     if (propertyName == "transition") {
         val transitions = value.split(",", ", ").filter { it.isNotBlank() }
         return transitions.map { Arg.Function.transition(it) }
     }
 
-    return value.split("\\s*(?![^()]*\\))[, ]\\s*".toRegex())// split on comma or space (or both?), but not in parens
-        .map { prop ->
-            if (prop == "0") {
-                return@map Arg.UnitNum(0, "px")
-            }
+    return splitString(value).map { prop ->
+        if (prop == "0") {
+            return@map Arg.UnitNum(0, "px")
+        }
 
-            val rawNum = prop.toIntOrNull() ?: prop.toDoubleOrNull()
-            if (rawNum != null) {
-                return@map Arg.RawNumber(rawNum)
-            }
+        val rawNum = prop.toIntOrNull() ?: prop.toDoubleOrNull()
+        if (rawNum != null) {
+            return@map Arg.RawNumber(rawNum)
+        }
 
-            if (prop.startsWith('#') || prop.startsWith("rgb")) {
-                Arg.Function.rgbOrNull(prop)?.let { return@map it }
-            }
+        if (prop.startsWith('#') || prop.startsWith("rgb")) {
+            Arg.Function.rgbOrNull(prop)?.let { return@map it }
+        }
 
-            val unit = Arg.UnitNum.ofOrNull(prop)
-            if (unit != null) {
-                return@map unit
-            }
+        val unit = Arg.UnitNum.ofOrNull(prop)
+        if (unit != null) {
+            return@map unit
+        }
 
-            if (prop.startsWith('"') || prop.startsWith("'")) {
-                return@map Arg.Literal(prop.replace("'", "\""))
-            }
+        if (prop.startsWith('"') || prop.startsWith("'")) {
+            return@map Arg.Literal(prop.replace("'", "\""))
+        }
 
-            val color = colors.firstOrNull { it.lowercase() == prop }
-            if (color != null) {
-                return@map Arg.Property("Colors", color)
-            }
+        val color = colors.firstOrNull { it.lowercase() == prop }
+        if (color != null) {
+            return@map Arg.Property("Colors", color)
+        }
 
         val className = when (propertyName) {
             "display" -> "DisplayStyle"
