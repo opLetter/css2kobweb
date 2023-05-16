d87d4b8a4563177dd48e4f9e069a5f90e885739e
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-05-15 20:15:57 -0400

    Use "size()" and "fillMaxSize()" if possible

--- parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/CSSParser.kt
+++ parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/CSSParser.kt
@@ -14,18 +14,27 @@ internal fun parseCss(css: String): List<Pair<String, ParsedModifier>> {
 
 internal fun getProperties(str: String): ParsedModifier {
     val props = str.split(";").map { it.trim() }.filter { it.isNotEmpty() }
-    return props.map { prop ->
+    return props.associate { prop ->
         val (name, value) = prop.split(":").map { it.trim() } + "" // use empty if not present
         val args = parseValue(
             propertyName = kebabToCamelCase(name),
             value = value.replace("\n", "").replace("  ", ""),
         )
-        ParsedProperty(function = kebabToCamelCase(name), args = args)
-    }.map {
+        kebabToCamelCase(name) to ParsedProperty(function = kebabToCamelCase(name), args = args)
+    }.let { properties ->
+        val width = properties["width"]?.args
+        val height = properties["height"]?.args
+        if (width != null && width.toString() == height.toString()) {
+            properties.filterKeys { it != "width" && it != "height" } +
+                    ("size" to ParsedProperty(function = "size", args = width))
+        } else properties
+    }.values.map {
         if (it.function == "width" && it.args.first().toString() == Arg.UnitNum(100, "percent").toString()) {
             ParsedProperty(function = "fillMaxWidth", args = emptyList())
         } else if (it.function == "height" && it.args.first().toString() == Arg.UnitNum(100, "percent").toString()) {
             ParsedProperty(function = "fillMaxHeight", args = emptyList())
+        } else if (it.function == "size" && it.args.first().toString() == Arg.UnitNum(100, "percent").toString()) {
+            ParsedProperty(function = "fillMaxSize", args = emptyList())
         } else it
     }.let { ParsedModifier(it) }
 }
