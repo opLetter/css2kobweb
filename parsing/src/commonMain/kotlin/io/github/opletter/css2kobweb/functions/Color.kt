72b2182273ba8034ab22cce13fa32aa09eac4d68
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-05-29 02:13:29 -0400

    Support hsl()

--- parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/functions/Color.kt
+++ parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/functions/Color.kt
@@ -2,12 +2,11 @@ package io.github.opletter.css2kobweb.functions
 
 import io.github.opletter.css2kobweb.Arg
 import io.github.opletter.css2kobweb.constants.colors
+import io.github.opletter.css2kobweb.parenContents
 import kotlin.math.roundToInt
 
 private fun rgbOrNull(prop: String): Arg.Function? {
-    val nums = prop.substringAfter("(").substringBefore(")")
-        .split(' ', ',', '/')
-        .filter { it.isNotBlank() }
+    val nums = parenContents(prop).split(' ', ',', '/').filter { it.isNotBlank() }
 
     val params = nums.take(3).map {
         if (it.endsWith("%")) Arg.Float(it.dropLast(1).toFloat() / 100)
@@ -27,6 +26,28 @@ private fun rgbOrNull(prop: String): Arg.Function? {
     return null
 }
 
+private fun hslOrNull(prop: String): Arg.Function? {
+    val nums = parenContents(prop).split(' ', ',', '/').filter { it.isNotBlank() }
+
+    val params = nums.take(3).mapIndexed { index, s ->
+        if (index == 0) {
+            Arg.UnitNum.ofOrNull(s, "deg") ?: Arg.UnitNum(s.toIntOrNull() ?: s.toDouble(), "deg")
+        } else {
+            Arg.UnitNum.ofOrNull(s, "percent") ?: Arg.Float(s.toFloat())
+        }
+    }
+    if (nums.size == 3) {
+        return Arg.Function("Color.hsl", params)
+    }
+    if (nums.size == 4) {
+        val alpha = nums.last().let {
+            Arg.UnitNum.ofOrNull(it, "percent") ?: Arg.Float(it.toFloat())
+        }
+        return Arg.Function("Color.hsla", params.take(3) + alpha)
+    }
+    return null
+}
+
 internal fun Arg.Companion.asColorOrNull(value: String): Arg? {
     if (value.startsWith("#")) {
         return Arg.Function("Color.rgb", listOf(Arg.Hex(value.drop(1))))
@@ -37,6 +58,9 @@ internal fun Arg.Companion.asColorOrNull(value: String): Arg? {
     }
     if (value.startsWith("rgb") && value.endsWith(")")) {
         return rgbOrNull(value)
+    }
+    if (value.startsWith("hsl") && value.endsWith(")")) {
+        return hslOrNull(value)
     }
     return null
 }
