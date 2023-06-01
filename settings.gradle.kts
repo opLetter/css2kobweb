146b0881981b5641414123ecf05ab21012feb663
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-06-01 13:01:23 -0400

    Support gradle build scan on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,20 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version ("3.9")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 rootProject.name = "css2kobweb"
 
 include(":site")
