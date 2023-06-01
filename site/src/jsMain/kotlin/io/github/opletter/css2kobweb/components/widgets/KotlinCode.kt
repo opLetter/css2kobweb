ef49e86a1489d66dff09ac542ef1083a4f8e660d
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-06-01 01:27:28 -0400

    Improve site performance Local benchmarks of rendering time: Small input: ~25ms -> ~5ms Large input: ~350ms -> ~30ms

--- site/src/jsMain/kotlin/io/github/opletter/css2kobweb/components/widgets/KotlinCode.kt
+++ site/src/jsMain/kotlin/io/github/opletter/css2kobweb/components/widgets/KotlinCode.kt
@@ -1,29 +1,27 @@
 package io.github.opletter.css2kobweb.components.widgets
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
+import com.varabyte.kobweb.compose.dom.ref
+import com.varabyte.kobweb.compose.dom.registerRefScope
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Color
-import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.toAttrs
-import com.varabyte.kobweb.silk.components.text.SpanText
 import io.github.opletter.css2kobweb.CodeBlock
 import io.github.opletter.css2kobweb.CodeElement
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Pre
-import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun KotlinCode(code: List<CodeBlock>, modifier: Modifier = Modifier) {
     Pre(Modifier.margin(0.px).then(modifier).toAttrs()) {
-        key(code.size) { // fixes performance issues
-            code.forEach { HighlightedCode(it) }
-        }
+        registerRefScope(ref(code) {
+            it.innerHTML = code.convertToHtml()
+        })
     }
 }
 
-object ColorScheme {
+private object ColorScheme {
     val keyword = Color.rgb(0xcF8E6D)
     val property = Color.rgb(0xC77DBB)
     val extensionFun = Color.rgb(0x56A8F5)
@@ -32,18 +30,22 @@ object ColorScheme {
     val namedArg = Color.rgb(0x56C1D6)
 }
 
-@Composable
-private fun HighlightedCode(text: CodeBlock) {
-    when (text.type) {
-        CodeElement.Keyword -> ColorScheme.keyword
-        CodeElement.Property -> ColorScheme.property
-        CodeElement.ExtensionFun -> ColorScheme.extensionFun
-        CodeElement.String -> ColorScheme.string
-        CodeElement.Number -> ColorScheme.number
-        CodeElement.NamedArg -> ColorScheme.namedArg
-        CodeElement.Plain -> {
-            Text(text.text)
-            return
-        }
-    }.let { SpanText(text.text, Modifier.color(it)) }
+private fun colorForType(type: CodeElement): Color = when (type) {
+    CodeElement.Keyword -> ColorScheme.keyword
+    CodeElement.Property -> ColorScheme.property
+    CodeElement.ExtensionFun -> ColorScheme.extensionFun
+    CodeElement.String -> ColorScheme.string
+    CodeElement.Number -> ColorScheme.number
+    CodeElement.NamedArg -> ColorScheme.namedArg
+    CodeElement.Plain -> error("Should not be plain")
 }
+
+private fun List<CodeBlock>.convertToHtml(): String = joinToString("") { block ->
+    if (block.type == CodeElement.Plain) {
+        block.text.htmlEscape()
+    } else {
+        """<span class="silk-span-text" style="color: ${colorForType(block.type)};">${block.text.htmlEscape()}</span>"""
+    }
+}
+
+private fun String.htmlEscape(): String = this.replace("&", "&amp;").replace("<", "&lt;")
