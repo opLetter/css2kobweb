20cc5046dd75d41d23e9a4498c7652caffb35521
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-10-19 19:03:24 -0400

    Add new shorthand properties

--- parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/constants/ShorthandProperties.kt
+++ parsing/src/commonMain/kotlin/io/github/opletter/css2kobweb/constants/ShorthandProperties.kt
@@ -11,6 +11,9 @@ val shorthandProperties = listOf(
     ShorthandProperty("overflow", listOf("X", "Y")),
     ShorthandProperty("paddingInline", listOf("Start", "End")),
     ShorthandProperty("paddingBlock", listOf("Start", "End")),
+    ShorthandProperty("padding", listOf("Top", "Right", "Bottom", "Left")),
+    ShorthandProperty("margin", listOf("Top", "Right", "Bottom", "Left")),
+    ShorthandProperty("font", listOf("Alternates", "Caps", "EastAsian", "Emoji", "Ligatures", "Numeric", "Settings")),
 ).flatMap { shortHand ->
     shortHand.subProperties.map { "${shortHand.property}$it" to it }
 }.toMap()
