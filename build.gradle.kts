3adb7c7055487979d07bae6622d55b7f07d7e07d
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-06 17:52:35 -0500

    Switch to `dependencyResolutionManagement` for repository config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,3 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
 }
-
-subprojects {
-    repositories {
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        google()
-        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,16 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+}
+
 plugins {
     id("com.gradle.enterprise") version ("3.13.3")
 }
