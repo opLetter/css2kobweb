09fcbbd7a0c732b945d4430b7c151e0062af566b
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-26 20:03:46 -0500

    Update gradle action

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,20 +16,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("com.gradle.enterprise") version ("3.13.3")
-}
-
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
-}
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "css2kobweb"
