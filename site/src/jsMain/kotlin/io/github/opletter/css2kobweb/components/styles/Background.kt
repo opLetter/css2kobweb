77b086c3706a725c28f104d10070cf19fc0d8c9b
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-03-22 16:24:54 -0400

    Nit: re-run css2kobweb on background image

--- site/src/jsMain/kotlin/io/github/opletter/css2kobweb/components/styles/Background.kt
+++ site/src/jsMain/kotlin/io/github/opletter/css2kobweb/components/styles/Background.kt
@@ -1,11 +1,11 @@
 package io.github.opletter.css2kobweb.components.styles
 
-import com.varabyte.kobweb.compose.css.BackgroundImage
 import com.varabyte.kobweb.compose.css.CSSBackground
 import com.varabyte.kobweb.compose.css.CSSPosition
 import com.varabyte.kobweb.compose.css.functions.RadialGradient
 import com.varabyte.kobweb.compose.css.functions.linearGradient
 import com.varabyte.kobweb.compose.css.functions.radialGradient
+import com.varabyte.kobweb.compose.css.functions.toImage
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.modifiers.background
@@ -15,62 +15,53 @@ import org.jetbrains.compose.web.css.deg
 import org.jetbrains.compose.web.css.percent
 
 // Image courtesy of gradientmagic.com
+// https://www.gradientmagic.com/collection/popular/gradient/1583693118025
 // translated with css2kobweb :)
 val BackgroundGradientStyle by ComponentStyle.base {
     Modifier
         .background(
             CSSBackground(
-                image = BackgroundImage.of(linearGradient(90.deg, Color.rgb(34, 222, 237), Color.rgb(135, 89, 215)))
+                image = linearGradient(90.deg, Color.rgb(34, 222, 237), Color.rgb(135, 89, 215)).toImage()
             ),
             CSSBackground(
-                image = BackgroundImage.of(
-                    radialGradient(RadialGradient.Shape.Circle, CSSPosition(75.percent, 99.percent)) {
-                        add(Color.rgba(243, 243, 243, 0.04f), 0.percent)
-                        add(Color.rgba(243, 243, 243, 0.04f), 50.percent)
-                        add(Color.rgba(37, 37, 37, 0.04f), 50.percent)
-                        add(Color.rgba(37, 37, 37, 0.04f), 100.percent)
-                    }
-                )
+                image = radialGradient(RadialGradient.Shape.Circle, CSSPosition(75.percent, 99.percent)) {
+                    add(Color.rgba(243, 243, 243, 0.04f), 0.percent)
+                    add(Color.rgba(243, 243, 243, 0.04f), 50.percent)
+                    add(Color.rgba(37, 37, 37, 0.04f), 50.percent)
+                    add(Color.rgba(37, 37, 37, 0.04f), 100.percent)
+                }.toImage()
             ),
             CSSBackground(
-                image = BackgroundImage.of(
-                    radialGradient(RadialGradient.Shape.Circle, CSSPosition(15.percent, 16.percent)) {
-                        add(Color.rgba(99, 99, 99, 0.04f), 0.percent)
-                        add(Color.rgba(99, 99, 99, 0.04f), 50.percent)
-                        add(Color.rgba(45, 45, 45, 0.04f), 50.percent)
-                        add(Color.rgba(45, 45, 45, 0.04f), 100.percent)
-                    }
-                )
+                image = radialGradient(RadialGradient.Shape.Circle, CSSPosition(15.percent, 16.percent)) {
+                    add(Color.rgba(99, 99, 99, 0.04f), 0.percent)
+                    add(Color.rgba(99, 99, 99, 0.04f), 50.percent)
+                    add(Color.rgba(45, 45, 45, 0.04f), 50.percent)
+                    add(Color.rgba(45, 45, 45, 0.04f), 100.percent)
+                }.toImage()
             ),
             CSSBackground(
-                image = BackgroundImage.of(
-                    radialGradient(RadialGradient.Shape.Circle, CSSPosition(86.percent, 7.percent)) {
-                        add(Color.rgba(40, 40, 40, 0.04f), 0.percent)
-                        add(Color.rgba(40, 40, 40, 0.04f), 50.percent)
-                        add(Color.rgba(200, 200, 200, 0.04f), 50.percent)
-                        add(Color.rgba(200, 200, 200, 0.04f), 100.percent)
-                    }
-                )
+                image = radialGradient(RadialGradient.Shape.Circle, CSSPosition(86.percent, 7.percent)) {
+                    add(Color.rgba(40, 40, 40, 0.04f), 0.percent)
+                    add(Color.rgba(40, 40, 40, 0.04f), 50.percent)
+                    add(Color.rgba(200, 200, 200, 0.04f), 50.percent)
+                    add(Color.rgba(200, 200, 200, 0.04f), 100.percent)
+                }.toImage()
             ),
             CSSBackground(
-                image = BackgroundImage.of(
-                    radialGradient(RadialGradient.Shape.Circle, CSSPosition(66.percent, 97.percent)) {
-                        add(Color.rgba(36, 36, 36, 0.04f), 0.percent)
-                        add(Color.rgba(36, 36, 36, 0.04f), 50.percent)
-                        add(Color.rgba(46, 46, 46, 0.04f), 50.percent)
-                        add(Color.rgba(46, 46, 46, 0.04f), 100.percent)
-                    }
-                )
+                image = radialGradient(RadialGradient.Shape.Circle, CSSPosition(66.percent, 97.percent)) {
+                    add(Color.rgba(36, 36, 36, 0.04f), 0.percent)
+                    add(Color.rgba(36, 36, 36, 0.04f), 50.percent)
+                    add(Color.rgba(46, 46, 46, 0.04f), 50.percent)
+                    add(Color.rgba(46, 46, 46, 0.04f), 100.percent)
+                }.toImage()
             ),
             CSSBackground(
-                image = BackgroundImage.of(
-                    radialGradient(RadialGradient.Shape.Circle, CSSPosition(40.percent, 91.percent)) {
-                        add(Color.rgba(251, 251, 251, 0.04f), 0.percent)
-                        add(Color.rgba(251, 251, 251, 0.04f), 50.percent)
-                        add(Color.rgba(229, 229, 229, 0.04f), 50.percent)
-                        add(Color.rgba(229, 229, 229, 0.04f), 100.percent)
-                    }
-                )
+                image = radialGradient(RadialGradient.Shape.Circle, CSSPosition(40.percent, 91.percent)) {
+                    add(Color.rgba(251, 251, 251, 0.04f), 0.percent)
+                    add(Color.rgba(251, 251, 251, 0.04f), 50.percent)
+                    add(Color.rgba(229, 229, 229, 0.04f), 50.percent)
+                    add(Color.rgba(229, 229, 229, 0.04f), 100.percent)
+                }.toImage()
             )
         )
 }
